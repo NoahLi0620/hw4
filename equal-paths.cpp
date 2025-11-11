a5175166f0abb269fe58498d3717adbd7f05131e
Noahli <tli09282@usc.edu>
2025-11-10 16:27:55 -0800

    first change

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,33 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+static bool checkDepth(Node* node,int depth,int& firstDepth)
+{
+    if(node==NULL)
+    {
+        return true;
+    }
+    if(node->left==NULL &&node->right==NULL)
+    {
+        if(firstDepth==-1)
+        {
+            firstDepth=depth;
+            return true;
+        }
+        return depth==firstDepth;
+    }
+    return checkDepth(node->left,depth+1,firstDepth)&&checkDepth(node->right,depth+1,firstDepth);
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if(root==NULL)
+    {
+        return true;
+    }
+    int firstDepth=-1;
+    return checkDepth(root,0,firstDepth);
 
 }
 
